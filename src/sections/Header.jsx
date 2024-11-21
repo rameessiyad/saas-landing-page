4babb444ee90f7301cdfce2ed62307ecc321cadd
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-21 15:42:57 +0530

    header ui updated

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -10,39 +10,43 @@ const NavLink = ({ title }) => (
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-10">
-      <div className="container flex items-center max-lg:px-5">
+      <div className="container flex items-center justify-between max-lg:px-5">
         <a href="" className="lg:hidden flex cursor-pointer z-2">
           <img src="/images/xora.svg" alt="logo" width={115} height={55} />
         </a>
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+          <div className="w-full max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                <li className="nav-li">
+                  <NavLink title="features" />
+                  <div className="dot" />
+                  <NavLink title="pricing" />
+                </li>
 
-        <div className="w-full border-2 border-amber-400">
-          <nav>
-            <ul className="flex max-lg:block max-lg:px-12">
-              <li className="nav-li">
-                <NavLink title="features" />
-                <div className="dot" />
-                <NavLink title="pricing" />
-              </li>
+                <li className="nav-logo">
+                  <LinkScroll>
+                    <img
+                      src="/images/xora.svg"
+                      alt="logo"
+                      width={160}
+                      height={55}
+                    />
+                  </LinkScroll>
+                </li>
 
-              <li className="nav-logo">
-                <LinkScroll>
-                  <img
-                    src="/images/xora.svg"
-                    alt="logo"
-                    width={160}
-                    height={55}
-                  />
-                </LinkScroll>
-              </li>
-
-              <li className="nav-li">
-                <NavLink title="faq" />
-                <div className="dot" />
-                <NavLink title="download" />
-              </li>
-            </ul>
-          </nav>
+                <li className="nav-li">
+                  <NavLink title="faq" />
+                  <div className="dot" />
+                  <NavLink title="download" />
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
+        <button className="lg:hidden size-10 z-2 border-2 border-s4/25  rounded-full flex justify-center items-center">
+          <img src="/images/magic.svg" alt="menu" />
+        </button>
       </div>
     </header>
   );
