e9d09bbf8fdd12922d0c31dea4efadca53d084e4
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-12-01 11:02:50 +0530

    feat: header done

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import clsx from "clsx";
 import { useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 
@@ -16,7 +17,12 @@ const Header = () => {
         <a href="" className="lg:hidden flex cursor-pointer z-2">
           <img src="/images/xora.svg" alt="logo" width={115} height={55} />
         </a>
-        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+        <div
+          className={clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
+          )}
+        >
           <div className="w-full max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
@@ -27,7 +33,15 @@ const Header = () => {
                 </li>
 
                 <li className="nav-logo">
-                  <LinkScroll>
+                  <LinkScroll
+                    to="hero"
+                    offset={-100}
+                    spy
+                    smooth
+                    className={clsx(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
+                    )}
+                  >
                     <img
                       src="/images/xora.svg"
                       alt="logo"
@@ -44,13 +58,34 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
+
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={280}
+                alt="oultine"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={280}
+                alt="oultine"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
+            </div>
           </div>
         </div>
         <button
           className="lg:hidden size-10 z-2 border-2 border-s4/25  rounded-full flex justify-center items-center"
           onClick={() => setisOpen((prev) => !prev)}
         >
-          <img src={`/images/${isOpen ? "close" : "magic"}.svg`} alt="menu" />
+          <img
+            src={`/images/${isOpen ? "close" : "magic"}.svg`}
+            alt="menu"
+            className="size-1/2 object-contain"
+          />
         </button>
       </div>
     </header>
