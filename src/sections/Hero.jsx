f74cbaf61ed7a725d6c3de634402c23c5ecf722a
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-12-01 20:27:48 +0530

    hero section text added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,26 @@
+import { Element } from "react-scroll";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="relative border-2 pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-amber-400">
+      <Element name="hero">
+        <div className="container">
+          <div className="relative z-2 max-w-512 max-lg:max-w-388">
+            <div className="caption small-2 uppercase text-p3 ">
+              Video Editing
+            </div>
+            <h1 className="mb-6 h1 text-p4 uppercase max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
+              Amazingly Simple
+            </h1>
+            <p className="max-w-440 mb-14 body-1 max-md:mb-10">
+              We designed Xora AI video editor to be easy to use, quick to learn
+              and surprisingly powerful.
+            </p>
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
 };
 
 export default Hero;
